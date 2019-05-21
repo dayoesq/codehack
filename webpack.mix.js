a35dc2cec9619ed01d8b80d65c59fa8ff5549430
Dayo <dayoesq@live.com>
2019-05-21 13:03:58 +0300

    Webpack added

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,20 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+mix.styles([
+    'resources/assets/css/libs/blog-post.css',
+    'resources/assets/css/libs/bootstrap.css',
+    'resources/assets/css/libs/font-awesome.css',
+    'resources/assets/css/libs/metisMenu.css',
+    'resources/assets/css/libs/sb-admin-2.css',
+    'resources/assets/css/libs/styles.css'
+], 'public/css/libs.css');
+
+mix.scripts([
+    'resources/assets/js/libs/bootstrap.js',
+    'resources/assets/css/libs/jquery.js',
+    'resources/assets/css/libs/metisMenu.js',
+    'resources/assets/css/libs/sb-admin-2.js',
+    'resources/assets/css/libs/scripts.js',
+    'resources/assets/css/libs/styles.css'
+], 'public/js/libs.js');
